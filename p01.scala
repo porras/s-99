b42789a1343cf1b23f1400f370fe75e7c9444812
Sergio Gil <sgilperez@gmail.com>
2016-09-08 16:41:07 +0200

    [P01] Return T instead of Option[T]

--- p01.scala
+++ p01.scala
@@ -1,12 +1,15 @@
+import java.util.NoSuchElementException
+
 object P01 {
-  def last[T](list: List[T]): Option[T] = list match {
-    case List() => None
-    case l :: Nil => Some(l)
+  def last[T](list: List[T]): T = list match {
+    case List() => throw new NoSuchElementException
+    case l :: Nil => l
     case _ :: t => last(t)
   }
 
   def example = {
     println(last(List(1, 1, 2, 3, 5, 8)))
     println(last(List("a", "b", "c")))
+    println(last(List()))
   }
 }
