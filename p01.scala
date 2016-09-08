d8986e05ff43236bec408b17b6cb233c4125c518
Sergio Gil <sgilperez@gmail.com>
2016-09-08 15:30:03 +0200

    [P01] Make it generic

--- p01.scala
+++ p01.scala
@@ -1,9 +1,12 @@
 object P01 {
-  def last(list: List[Int]): Option[Int] = list match {
+  def last[T](list: List[T]): Option[T] = list match {
     case List() => None
     case l :: Nil => Some(l)
     case _ :: t => last(t)
   }
 
-  def example = println(last(List(1, 1, 2, 3, 5, 8)))
+  def example = {
+    println(last(List(1, 1, 2, 3, 5, 8)))
+    println(last(List("a", "b", "c")))
+  }
 }
