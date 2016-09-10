b18d275eb4d7a7b599c02d128d3aeeb657b64d07
Sergio Gil <sgilperez@gmail.com>
2016-09-10 16:31:12 +0200

    [P03] nth

---
+++ p03.scala
@@ -0,0 +1,15 @@
+import java.util.NoSuchElementException
+
+object P03 {
+  def nth[T](n: Int, list: List[T]): T = (n, list) match {
+    case (_, Nil) => throw new NoSuchElementException
+    case (0, h :: _) => h
+    case (_, _ :: tail) => nth(n - 1, tail)
+  }
+
+  def example = {
+    println(nth(0, List(1, 2, 3, 5, 8)))
+    println(nth(1, List("a", "b", "c")))
+    println(nth(2, List(1)))
+  }
+}
