57752f5648242739f239e63d44b3091428726f7b
Sergio Gil <sgilperez@gmail.com>
2016-09-14 15:24:48 +0200

    [P03] Add @tailrec annotation

--- p03.scala
+++ p03.scala
@@ -1,6 +1,9 @@
 import java.util.NoSuchElementException
 
+import scala.annotation.tailrec
+
 object P03 {
+  @tailrec
   def nth[T](n: Int, list: List[T]): T = (n, list) match {
     case (_, Nil) => throw new NoSuchElementException
     case (0, h :: _) => h
