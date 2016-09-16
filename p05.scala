f4e0171757b8aa3ee6c8db0500127e816e597339
Sergio Gil <sgilperez@gmail.com>
2016-09-16 11:02:16 +0200

    [P05] make reverse tail recursive

--- p05.scala
+++ p05.scala
@@ -1,13 +1,19 @@
+import scala.annotation.tailrec
+
 object P05 {
-  def reverse[T](list: List[T]): List[T] = list match {
-    case List() => List()
-    case h :: Nil => List(h)
-    case h :: t => reverse(t) ::: List(h)
+  @tailrec
+  def reverse[T](list: List[T], acc: List[T] = List()): List[T] = list match {
+    case Nil => acc
+    case h :: Nil => h :: acc
+    case h :: t => reverse(t, h :: acc)
   }
 
   def example = {
-    println(reverse(List(1, 2, 3, 5, 8)))
-    println(reverse(List("a", "b", "c")))
-    println(reverse(List()))
+    assert(reverse(List(1, 2, 3, 5, 8)) == List(8, 5, 3, 2, 1))
+    assert(reverse(List("a", "b", "c")) == List("c", "b", "a"))
+    assert(reverse(List(2)) == List(2))
+    assert(reverse(List()) == List())
+
+    println("alles gut!")
   }
 }
