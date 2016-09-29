04373fbcfcac4fd3f6aa7377202e5dbe957fae14
Sergio Gil <sgilperez@gmail.com>
2016-09-29 16:51:01 +0200

    [P10] encode

--- p10.scala
+++ p10.scala
@@ -1,8 +1,11 @@
 import scala.annotation.tailrec
 
-object P09 {
+object P10 {
+  // TODO: use P04.length (instead of stdlib's .size) and P09.pack (instead of copying and pasting it here)
+  def encode[T](list: List[T]): List[(Int, T)] = pack(list).map(x => (x.size, x.head))
+
   @tailrec
-  def pack[T](list: List[T], current: List[T] = List(), result: List[List[T]] = List.empty): List[List[T]] = (list, current) match {
+  private def pack[T](list: List[T], current: List[T] = List(), result: List[List[T]] = List.empty): List[List[T]] = (list, current) match {
     case (h :: t, Nil) => pack(t, List(h), result)
     case (h :: t, c :: _) if h == c => pack(t, c :: current, result)
     case (h :: t, c :: _) => pack(t, List(h), current :: result)
@@ -10,7 +13,7 @@ object P09 {
   }
 
   def example = {
-    assert(pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+    assert(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
 
     println("alles gut!")
   }
