0e021ee9fc299a76aaef371db9f34dfb9b8443b7
Sergio Gil <sgilperez@gmail.com>
2016-10-04 17:47:41 +0200

    Implement equality

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -1,8 +1,17 @@
 object P11 {
-  trait EncodedItem[T]
-  case class One[T](x : T) extends EncodedItem[T]
-  case class Multi[T](t : (Int, T)) extends EncodedItem[T]
-  
+  trait EncodedItem[T] {
+    def equals(o: T) = false
+    def equals(o: (Int, T)) = false
+  }
+
+  case class One[T](x : T) extends EncodedItem[T] {
+    override def equals(o: T) = x == o
+  }
+
+  case class Multi[T](t : (Int, T)) extends EncodedItem[T] {
+    override def equals(o: (Int, T)) = t == o
+  }
+
   def encodeModified[T](list: List[T]): List[EncodedItem[T]] = P09.pack(list).map {
     case onlyItem :: Nil => One(onlyItem)
     case h :: t => Multi((P04.length(t) + 1, h))
