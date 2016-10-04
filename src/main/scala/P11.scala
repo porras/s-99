3df86cdd3a36e14a4834503107a2ae5eae781065
Sergio Gil <sgilperez@gmail.com>
2016-10-04 17:44:23 +0200

    Use a trait

--- src/main/scala/P11.scala
+++ src/main/scala/P11.scala
@@ -1,6 +1,10 @@
 object P11 {
-  def encodeModified[T](list: List[T]): List[Any] = P09.pack(list).map {
-    case onlyItem :: Nil => onlyItem
-    case h :: t => (P04.length(t) + 1, h)
+  trait EncodedItem[T]
+  case class One[T](x : T) extends EncodedItem[T]
+  case class Multi[T](t : (Int, T)) extends EncodedItem[T]
+  
+  def encodeModified[T](list: List[T]): List[EncodedItem[T]] = P09.pack(list).map {
+    case onlyItem :: Nil => One(onlyItem)
+    case h :: t => Multi((P04.length(t) + 1, h))
   }
 }
--- src/test/scala/P11Spec.scala
+++ src/test/scala/P11Spec.scala
@@ -1,7 +1,8 @@
+import P11.{One, Multi}
 import org.specs2.mutable.Specification
 
 object P11Spec extends Specification {
   "encodeModified" >> {
-    P11.encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==== List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+    P11.encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) ==== List(Multi((4,'a)), One('b), Multi((2,'c)), Multi((2,'a)), One('d), Multi((4,'e)))
   }
 }
