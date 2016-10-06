0896f6aec8f2e21c60f89299d76bf471de6d7e94
Sergio Gil <sgilperez@gmail.com>
2016-10-06 10:23:14 +0200

    [P12] decode with for comprehension

--- src/main/scala/P12.scala
+++ src/main/scala/P12.scala
@@ -1,10 +1,8 @@
-import scala.annotation.tailrec
-
 object P12 {
-  @tailrec
-  def decode[T](encodedList: List[(Int, T)], decodedList: List[T] = List()): List[T] = encodedList match {
-    case (1, x) :: t => decode(t, x :: decodedList)
-    case (i, x) :: t => decode((i - 1, x) :: t, x :: decodedList)
-    case List() => P05.reverse(decodedList)
+  def decode[T](encodedList: List[(Int, T)]): List[T] = {
+    for {
+      (n, c) <- encodedList
+      _ <- 1 to n
+    } yield c
   }
 }
